a07ff470806aff6e96f696e12421d37e483d008e
Kirill <45152125+Krealll@users.noreply.github.com>
2020-05-28 05:59:13 +0300

    Delete DivisionService.java

--- RestLab1/DivisionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.restservice;
-
-public class DivisionService {
-
-    Division divide(Integer dividend, Integer divider){
-        return new Division(dividend/divider, dividend%divider);
-    }
-
-}
